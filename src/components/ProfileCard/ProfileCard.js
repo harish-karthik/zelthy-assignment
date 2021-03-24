4c348094a3ac7aee7da3d9bc5216b12a1c347868
Harish Karthik <hhkk0010@outlook.com>
2021-03-24 09:11:26 +0530

    added icons for the profile cards

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ProfileImage from "../ProfileImage/ProfileImage";
+import { ReactComponent as PhoneIconComponent } from "../../assets/icons/phone-solid.svg";
+import { ReactComponent as EnvelopeIconComponent } from "../../assets/icons/envelope.svg";
+import { ReactComponent as GlobeIconComponent } from "../../assets/icons/globe-solid.svg";
 import "./ProfileCard.css";
 
 function ProfileCard(props) {
@@ -10,9 +13,24 @@ function ProfileCard(props) {
         <ProfileImage username={username} />
         <div className='card__textcontainer'>
           <h1 className='card__title'>{name}</h1>
-          <p className='card__text'>{phone}</p>
-          <p className='card__text'>{email}</p>
-          <p className='card__text'>{website}</p>
+          <p className='card__text'>
+            <span className='card__icon'>
+              <EnvelopeIconComponent />
+            </span>
+            {email}
+          </p>
+          <p className='card__text'>
+            <span className='card__icon'>
+              <PhoneIconComponent />
+            </span>
+            {phone}
+          </p>
+          <p className='card__text'>
+            <span className='card__icon'>
+              <GlobeIconComponent />
+            </span>
+            {website}
+          </p>
         </div>
       </div>
     </article>
